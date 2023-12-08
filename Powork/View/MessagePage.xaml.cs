f7ea0da372a4a73b5950616ca8c3b4d62cecb1ce
ZjzMisaka <530514257@qq.com>
2023-12-08 23:52:15 +0800

    update

--- Powork/View/MessagePage.xaml.cs
+++ Powork/View/MessagePage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Powork.ViewModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,19 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Wpf.Ui.Controls;
 
-namespace Powork
+namespace Powork.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MessagePage.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : FluentWindow
+    public partial class MessagePage : Page
     {
-        public MainWindow()
+        public MessagePage()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowViewModel();
         }
     }
 }
