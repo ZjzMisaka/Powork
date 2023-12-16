e41eab22d22e5e6217d4e81add2fc3f312f58652
ZjzMisaka <zjzmisaka@outlook.com>
2023-12-16 16:26:18 +0800

    Evidence model

---
+++ Powork/Model/Evidence/Block.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Powork.Model.Evidence
+{
+    public class Block
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ImageSource ImageSource { get; set; }
+        public List<Shape> ShapeList { get; set; }
+    }
+}
---
+++ Powork/Model/Evidence/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powork.Model.Evidence
+{
+    public enum Type 
+    {
+        EmptyRectangle,
+        FilledRectangle,
+        Line,
+    }
+    public class Shape
+    {
+        public Type Type { get; set; }
+        public string Content { get; set; }
+        public Point Position { get; set; }
+    }
+}
