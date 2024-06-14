2b0da6e479c8f594657ed28836ce163ac42a4d72
ZjzMisaka <zjzmisaka@outlook.com>
2024-06-14 13:18:23 +0800

    update

--- Powork/Helper/WindowHelper.cs
+++ Powork/Helper/WindowHelper.cs
@@ -1,12 +1,30 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace Powork.Helper
 {
     public static class WindowHelper
     {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
         public static bool IsWindowActive(Window window)
         {
-            return window.WindowState != WindowState.Minimized && window.Visibility == Visibility.Visible;
+            IntPtr foregroundWindow = GetForegroundWindow();
+            if (foregroundWindow == IntPtr.Zero)
+            {
+                return false;
+            }
+            GetWindowThreadProcessId(foregroundWindow, out uint processID);
+
+            int currentProcessID = Process.GetCurrentProcess().Id;
+            return window.WindowState != WindowState.Minimized &&
+                window.Visibility == Visibility.Visible &&
+                 processID == currentProcessID;
         }
     }
 }
